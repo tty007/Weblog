32ccfc4dce79e63404068a3b88aaf2f490efa93d
tty007 <c7dc007@gmail.com>
2018-09-08 13:23:56 +0900

    devise-implementation

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         #authenticationに使うキーを設定する
+        #  :authentication_keys[];
+  validates :name, presence: true, uniqueness: true
+
+  #登録時にメールアドレスを不要にする（オーバーライド）
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+end
